32fa2810930f60edf1e9accd7d32fcd0f103ac41
Mramaizng <beersbr@gmail.com>
2015-01-16 11:52:21 -0800

    updated paths
    updated paths

--- misc/shell.bat
+++ misc/shell.bat
@@ -2,6 +2,6 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC/vcvarsall" x64
 
-set path="C:\Users\brett\Desktop\game_from_scratch\code\misc";%path%
+set path="C:\Users\brett\Desktop\scratch\code\misc";%path%
 
-"D:\Program Files\Sublime Text 2\sublime_text.exe" C:\Users\brett\Desktop\game_from_scratch
+"D:\Program Files\Sublime Text 2\sublime_text.exe" C:\Users\brett\Desktop\scratch
