b58b0a5640b0a00d365f844979064681724a8785
Mramaizng <beersbr@gmail.com>
2015-01-16 23:25:44 -0800

    shell script
    fixed the shell script

--- misc/shell.bat
+++ misc/shell.bat
@@ -4,4 +4,4 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC/vcvarsall" x64
 
 set path="C:\Users\brett\Desktop\scratch\code\misc";%path%
 
-"D:\Program Files\Sublime Text 2\sublime_text.exe" C:\Users\brett\Desktop\scratch
+start "" "D:\Program Files\Sublime Text 2\sublime_text.exe" "C:\Users\brett\Desktop\scratch"
